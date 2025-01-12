cfb3701b43d6ed5147e1a1bff6b71125e8a2052a
Bayu Adji Prakoso <bayuadjiprakoso@gmail,com>
2025-01-13 01:16:45 +0700

    on progress

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ ScrollReveal({
   duration: 2000,
   delay: 200,
 });
+
+/*scroll*/
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".axion-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .skills-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
